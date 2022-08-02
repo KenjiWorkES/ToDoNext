d7fdf1c47babf33602440f611c22ed1de9d44630
KenjiLucas <802.182@alunos.unigran.br>
2022-08-02 08:53:03 -0400

    create function to press enter

--- components/tasks/AddTask.jsx
+++ components/tasks/AddTask.jsx
@@ -1,9 +1,17 @@
 const AddTask = () => {
+  const onEnterHandler = (event) => {
+    if (event.key === "Enter") {
+      console.log("Press Enter");
+      event.preventDefault();
+    }
+  };
+
   return (
     <div className="header__control">
       <input id="addCheck" className="header__check" type="checkbox" />
       <label className="header__label" htmlFor="addCheck"></label>
       <input
+        onKeyDown={onEnterHandler}
         className="header__input"
         type="text"
         placeholder="Create a new todo..."
