b7b5b7ea19aa89455f85a1a278350c9e7af5e94f
KenjiLucas <802.182@alunos.unigran.br>
2022-08-02 09:03:26 -0400

    create function to salve input value in a state

--- components/tasks/AddTask.jsx
+++ components/tasks/AddTask.jsx
@@ -1,20 +1,39 @@
+import { useRef, useState } from "react";
+
 const AddTask = () => {
+  const checkRef = useRef();
+
+  const [enteredTask, setEnteredTask] = useState("");
+
   const onEnterHandler = (event) => {
     if (event.key === "Enter") {
-      console.log("Press Enter");
       event.preventDefault();
+      setEnteredTask("");
+      console.log(checkRef.current.checked);
     }
+  };
+
+  const onTypeHandler = (event) => {
+    console.log(event.target.value);
+    setEnteredTask(event.target.value);
   };
 
   return (
     <div className="header__control">
-      <input id="addCheck" className="header__check" type="checkbox" />
+      <input
+        id="addCheck"
+        className="header__check"
+        type="checkbox"
+        ref={checkRef}
+      />
       <label className="header__label" htmlFor="addCheck"></label>
       <input
         onKeyDown={onEnterHandler}
         className="header__input"
         type="text"
         placeholder="Create a new todo..."
+        onChange={onTypeHandler}
+        value={enteredTask}
       />
     </div>
   );
