ea9beff46cc9fab30abef35cf236d5db2f483c18
KenjiLucas <802.182@alunos.unigran.br>
2022-08-03 16:35:04 -0400

    add onDrag end to the list

--- components/tasks/TasksList.jsx
+++ components/tasks/TasksList.jsx
@@ -27,8 +27,30 @@ const TaskList = ({ items }) => {
     }
   });
 
+  const dragEndHandler = (result) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const taskList = reorder(
+      ctx.tasksList,
+      result.source.index,
+      result.destination.index
+    );
+
+    ctx.setTaskList(taskList);
+  };
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+  };
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={dragEndHandler}>
       <Droppable droppableId="tasks">
         {(provided) => (
           <ul
